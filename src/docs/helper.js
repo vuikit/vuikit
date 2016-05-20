82680a7ef397da4cb076ff2012aa99f103aaa2fa
Miljan Aleksic <miljan@aleksic.es>
2016-05-20 12:18:32 +0700

    add warning if trying to get props from unregistered component

--- src/docs/helper.js
+++ src/docs/helper.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import { each, merge } from 'lodash'
 
 export const getProps = (component, demo) => {
+  if (!Vue.options.components[`Vk${component}`]) {
+    return Vue.util.warn(`Vk${component} not found, did you registered it?`)
+  }
   const props = merge({},
     Vue.options.components[`Vk${component}`].options.props,
     demo
