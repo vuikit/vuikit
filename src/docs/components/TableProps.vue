50894657e5a51e4cb61dc3a4f4a7fd4f9e71c6b3
vpetrenko <vpetrenko@mindk.com>
2016-06-03 16:26:51 +0300

    add responsive tables

--- src/docs/components/TableProps.vue
+++ src/docs/components/TableProps.vue
@@ -3,21 +3,21 @@
     <thead>
       <tr>
         <th>Name</th>
-        <th>Type</th>
-        <th>Default</th>
-        <th>Description</th>
-        <th v-if="demo">Demo</th>
-      </tr>
+        <th class="tm-docs-hidden-medium">Type</th>
+        <th class="tm-docs-hidden-medium">Default</th>
+        <th class="tm-docs-hidden-small">Description</th>
+        <th class="tm-docs-hidden-small" v-if="demo">Demo</th>
+    </tr>
     </thead>
     <tbody>
       <tr v-else class="uk-table-middle" v-for="row in sortedRows">
         <td v-text="row.name"></td>
-        <td v-text="row.type | type"></td>
-        <td class="uk-text-truncate">
+        <td class="tm-docs-hidden-medium" v-text="row.type | type"></td>
+        <td class="tm-docs-hidden-medium uk-text-truncate">
           <code v-text="row.default | default"></code>
         </td>
-        <td v-html="row.description"></td>
-        <td v-if="demo" is="PropsColDemo"
+        <td class="tm-docs-hidden-small" v-html="row.description"></td>
+        <td class="tm-docs-hidden-small" v-if="demo" is="PropsColDemo"
           :editable="row.editable"
           :options="row.options"
           :type="row.type | type"
