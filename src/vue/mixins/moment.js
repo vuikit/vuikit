d53514492a986c46cd08190f7759720df8b72867
Miljan Aleksic <miljan@aleksic.es>
2016-06-06 14:19:36 +0800

    fix moment locale

--- src/vue/mixins/moment.js
+++ src/vue/mixins/moment.js
@@ -1,5 +1,10 @@
-import Moment from 'moment'
 import Vue from 'vue'
+import Moment from 'moment'
+import UUID from '../helpers/uuid'
+
+function localeId (prefix) {
+  return UUID.short(`${prefix}-`)
+}
 
 export default {
   init () {
@@ -8,18 +13,19 @@ export default {
       this.$destroy()
     } else {
       this.$moment = function (date) {
-        return (date && Moment(date, this.format, true).isValid()
+        const moment = date && Moment(date, this.format, true).isValid()
           ? Moment(date, this.format, true)
           : Moment()
-        ).locale(`${this.$options.name}_${this._uid}`)
+        moment.locale(localeId(this.$options.name))
+        return moment
       }
     }
   },
   created () {
-    // define custom locale at created
-    // as we need access to the local object
+    // define custom locale at created event
+    // as we need to access to props
     const globalLocale = Moment.locale()
-    Moment.defineLocale(`${this.$options.name}_${this._uid}`, {
+    Moment.defineLocale(localeId(this.$options.name), {
       parentLocale: 'en',
       months: this.locale.months,
       weekdaysShort: this.locale.weekDays,
