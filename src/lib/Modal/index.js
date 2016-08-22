9ca1ca6e9d5995f4bd8cfb7d96e9401abf0a3ab8
Miljan Aleksic <miljan@aleksic.es>
2016-08-22 22:20:16 +0900

    refactor Modal

--- src/lib/Modal/index.js
+++ src/lib/Modal/index.js
@@ -1,10 +1,5 @@
-import { debounce } from 'lodash'
-import { on, offAll, css, addClass, removeClass } from '../../helpers/dom'
+import { on, offAll } from '../../helpers/dom'
 import render from './render'
-
-// active modal ref
-let currentlyActive
-let activeCount
 
 export default {
   name: 'VkModal',
@@ -35,114 +30,23 @@ export default {
       default: 'vk-modal-transition'
     }
   },
-  computed: {
-    languageDir () {
-      const html = document.documentElement
-      return html.getAttribute('dir') === 'rtl'
-        ? 'right'
-        : 'left'
-    },
-    paddingDir () {
-      return 'padding-' + (this.languageDir === 'left'
-        ? 'right'
-        : 'left')
-    }
-  },
   mounted () {
-    this.initEvents()
-  },
-  methods: {
-    beforeEnter () {
-      const html = document.documentElement
-      this.$nextTick(() => {
-        addClass(html, 'uk-modal-page')
-        this.resize(true)
-      })
-    },
-    afterEnter () {
-      if (currentlyActive) {
-        currentlyActive.$emit('inactive')
-      }
-      currentlyActive = this
-    },
-    afterLeave () {
-      const html = document.documentElement
-      const body = document.body
-      activeCount > 0
-        ? activeCount--
-        : activeCount = 0
-      if (!activeCount) {
-        removeClass(html, 'uk-modal-page')
-        body.style[this.paddingDir] = ''
-      }
-      if (currentlyActive === this) {
-        currentlyActive = false
-      }
-    },
-    // just a shortcut to avoid setting up
-    // the namespace every time
-    on (el, event, handler) {
-      on(el, event, handler, this._uid)
-    },
-    initEvents () {
-      // window events
-      const windowEvents = ['resize', 'orientationchange']
-      const windowHandler = debounce(event => {
-        this.resize()
-      }, 150)
-      for (let i = 0; i < windowEvents.length; ++i) {
-        this.on(window, windowEvents[i], windowHandler)
-      }
-      // keyboard events
-      this.on(document, 'keyup', (event) => {
-        if (this.show && event.keyCode === 27) {
-          event.preventDefault()
-          this.$emit('keyEsc')
-        }
-      })
-      // click events
-      let clickEvents = ['click']
-      if ('ontouchstart' in document.documentElement) {
-        clickEvents.push('touchstart')
-      }
-      const clickHandler = event => {
-        // click in/out modal dialog
-        if (event.target === this.$refs.dialog || this.$refs.dialog.contains(event.target)) {
-          this.$emit('clickIn', event)
-        } else {
-          this.$emit('clickOut', event)
-        }
-      }
-      for (let i = 0; i < clickEvents.length; ++i) {
-        this.on(this.$el, clickEvents[i], clickHandler)
-      }
-    },
-    resize (force) {
-      if (!this.show && !force) {
-        return
-      }
-      const body = document.body
-      const bodyWidth = body.offsetWidth
-      const scrollbarWidth = window.innerWidth - bodyWidth
-      const dialog = this.$refs.dialog
-      body.style[this.paddingDir] = scrollbarWidth
-      this.$el.style['overflow-y'] = scrollbarWidth
-        ? 'scroll'
-        : 'auto'
-      if (this.center) {
-        const dh = dialog.offsetHeight
-        const marginTop = css(dialog, 'margin-top')
-        const marginBottom = css(dialog, 'margin-bottom')
-        const pad = parseInt(marginTop, 10) + parseInt(marginBottom, 10)
-        if ((dh + pad) < window.innerHeight) {
-          const top = (window.innerHeight / 2 - dh / 2) - pad
-          dialog.style.top = `${top}px`
-        } else {
-          dialog.style.top = ''
-        }
+    // init click events
+    const dialog = this.$el.childNodes[0]
+    const clickEvents = ['click']
+    if ('ontouchstart' in document.documentElement) {
+      clickEvents.push('touchstart')
+    }
+    const clickHandler = event => {
+      // click in/out modal dialog
+      if (event.target === dialog || dialog.contains(event.target)) {
+        this.$emit('clickIn', event)
       } else {
-        dialog.style.top = ''
+        this.$emit('clickOut', event)
       }
+    }
+    for (let i = 0; i < clickEvents.length; ++i) {
+      on(this.$el, clickEvents[i], clickHandler, this._uid)
     }
   },
   beforeDestroy () {
--- src/lib/Modal/render.js
+++ src/lib/Modal/render.js
@@ -1,29 +1,23 @@
+import Transition from './Transition'
+
 export default function (h) {
   const directives = [{
     name: 'show', value: this.show
   }]
-  return (
-    <transition name={ this.transition } {...{
-      on: {
-        'before-enter': this.beforeEnter,
-        'after-enter': this.afterEnter,
-        'after-leave': this.afterLeave
-      }
-    }}>
-      <div {...{ directives }} class="uk-modal"
-        aria-hidden={ this.show
-          ? 'false'
-          : 'true'
-        }>
-        <div ref="dialog" class={{
-          'uk-modal-dialog': true,
-          'uk-modal-dialog-large': this.large,
-          'uk-modal-dialog-lightbox': this.lightbox,
-          'uk-modal-dialog-blank': this.blank
-        }}>
-          { this.$slots.default }
-        </div>
+  const modal = (
+    <div {...{ directives }} class="uk-modal"
+      aria-hidden={ this.show
+        ? 'false'
+        : 'true'
+      }>
+      <div staticClass="uk-modal-dialog" class={{
+        'uk-modal-dialog-large': this.large,
+        'uk-modal-dialog-lightbox': this.lightbox,
+        'uk-modal-dialog-blank': this.blank
+      }}>
+        { this.$slots.default }
       </div>
-    </transition>
+    </div>
   )
+  return h(Transition, [ modal ])
 }
