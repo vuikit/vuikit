300ef8eb16c9a61ea264ebc898d0f6deaebe5c65
Miljan Aleksic <miljan@aleksic.es>
2016-11-24 14:29:05 +0800

    cleanup Datepicker

--- src/lib/Datepicker.js
+++ src/lib/Datepicker.js
@@ -1,9 +1,7 @@
 import Moment from 'moment'
-import render from './render'
 
 export default {
   name: 'VkDatepicker',
-  render,
   props: {
     year: {},
     month: {},
@@ -25,6 +23,16 @@ export default {
       type: [String, Number],
       default: '2050-12-31'
     }
+  },
+  render (h) {
+    return (
+      <vk-calendar
+        year={ this.year }
+        month={ this.month }
+        date-render={ DateRender }
+        on-change={date => this.$emit('change', date)}>
+      </vk-calendar>
+    )
   },
   computed: {
     datesMoments () {
@@ -60,4 +68,23 @@ export default {
         : this.$emit('pick', date)
     }
   }
+}
+
+const DateRender = function (h, { props, parent }) {
+  const $calendar = parent
+  const $datepicker = parent.$parent
+  return (
+    <a class={{
+      'uk-active': $datepicker.isPicked(props.date),
+      'uk-datepicker-table-disabled': $datepicker.isDisabled(props.date),
+      'uk-datepicker-table-muted': props.date.month() !== $calendar.month ||
+        $datepicker.isDisabled(props.date)
+    }} on-click={() => {
+      if (!$datepicker.isDisabled(props.date)) {
+        $datepicker.toggle(props.date)
+      }
+    }}>
+      { props.date.format('D') }
+    </a>
+  )
 }
