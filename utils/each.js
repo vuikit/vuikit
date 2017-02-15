401d0936f433c18e7228acba118b373a1ec69634
Miljan Aleksic <miljan@aleksic.es>
2017-02-15 18:55:48 +0800

    add each util

---
+++ utils/each.js
@@ -0,0 +1,18 @@
+const isObject = require('../utils/is-object')
+const isInteger = require('../utils/is-integer')
+
+module.exports = function (obj, iterator) {
+  var i, key
+  if (isInteger(obj.length)) {
+    for (i = 0; i < obj.length; i++) {
+      iterator.call(obj[i], obj[i], i)
+    }
+  } else if (isObject(obj)) {
+    for (key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        iterator.call(obj[key], obj[key], key)
+      }
+    }
+  }
+  return obj
+}
