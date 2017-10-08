aa1b32f0d6f3452bf93f22ce920bfcb96d55dea9
Miljan Aleksic <miljan@aleksic.es>
2017-10-08 12:20:12 +0800

    add ui-button-group

---
+++ src/ui/button/button-group.js
@@ -0,0 +1,6 @@
+export default {
+  functional: true,
+  render (h, { children }) {
+    return <div class="uk-button-group">{ children }</div>
+  }
+}
--- src/ui/button/story.vue
+++ src/ui/button/story.vue
@@ -1,29 +1,38 @@
 <template>
   <div class="uk-padding">
-    <h1>Button</h1>
+    <h2>Button</h2>
     <ui-button>Default</ui-button>
     <ui-button disabled>Disabled</ui-button>
 
-    <h2>Type</h2>
+    <h3>Type</h3>
     <ui-button type="primary">Primary</ui-button>
     <ui-button type="secondary">Secondary</ui-button>
     <ui-button type="danger">Danger</ui-button>
     <ui-button type="text">Text</ui-button>
     <ui-button type="link">Link</ui-button>
 
-    <h2>Size</h2>
+    <h3>Size</h3>
     <ui-button size="large">Large</ui-button>
     <ui-button>Default</ui-button>
     <ui-button size="small">Small</ui-button>
+
+    <h2>Button Group</h2>
+    <ui-button-group>
+      <ui-button>Buton</ui-button>
+      <ui-button>Buton</ui-button>
+      <ui-button>Buton</ui-button>
+    </ui-button-group>
   </div>
 </template>
 
 <script>
 import UiButton from './button'
+import UiButtonGroup from './button-group'
 
 export default {
   components: {
-    UiButton
+    UiButton,
+    UiButtonGroup
   }
 }
 </script>
