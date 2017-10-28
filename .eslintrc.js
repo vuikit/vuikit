8fa91d095ec13013a9d1b01e0fb1939eaeda5e21
Miljan Aleksic <miljan@aleksic.es>
2017-10-28 23:57:09 +0800

    update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,11 @@
 module.exports = {
   root: true,
+  parser: 'babel-eslint',
   parserOptions: {
     sourceType: 'module'
+  },
+  env: {
+    browser: true,
   },
   // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
   extends: 'standard',
