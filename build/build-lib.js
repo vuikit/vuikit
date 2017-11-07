12325b98f4e40d97fd978ecbd103ca0fd53f29d2
Miljan Aleksic <miljan@aleksic.es>
2017-11-08 00:33:44 +0800

    add lib build

---
+++ build/build-lib.js
@@ -0,0 +1,54 @@
+import path from 'path'
+import globby from 'globby'
+import alias from './alias'
+
+import lumpit from '@lump/it'
+import argv from '@lump/argv'
+import write from '@lump/write'
+import rollup from '@lump/rollup'
+
+import vue from 'rollup-plugin-vue'
+import buble from 'rollup-plugin-buble'
+import replace from 'rollup-plugin-replace'
+import rollupAlias from 'rollup-plugin-alias'
+import nodeResolve from 'rollup-plugin-node-resolve'
+
+const args = argv()
+
+lumpit(async () => {
+  const components = await globby.sync('src/components/*/index.js')
+  await Promise.all(components.map(compile))
+
+  const directives = await globby.sync('src/directives/*/index.js')
+  await Promise.all(directives.map(compile))
+})
+
+async function compile (input) {
+  const dirname = path.dirname(input)
+  const basename = dirname.split('/').pop()
+
+  let { code } = await rollup({
+    input,
+    output: {
+      format: 'es'
+    },
+    external: id => id.match(/@vuikit\/core/) || id === 'vue',
+    plugins: [
+      nodeResolve({
+        extensions: [ '.js', '.json', '.vue' ]
+      }),
+      vue({
+        compileTemplate: true
+      }),
+      buble(),
+      rollupAlias(alias),
+      replace({
+        'process.env.NODE_ENV': JSON.stringify('development')
+      })
+    ]
+  }, {
+    report: args.report
+  })
+
+  await write(`lib/${basename}.js`, code, { log: true })
+}
