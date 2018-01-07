b3d220b5d8663a84b284a84d0b0338313bd77a0e
Miljan Aleksic <miljan@aleksic.es>
2018-01-07 18:07:52 +0800

    remove vk-button-group-*
    Those are not part of uikit and even though vuikit could support it, it should be better implemented, with custom styles if required.

--- packages/vuikit/src/components/button/index.js
+++ packages/vuikit/src/components/button/index.js
@@ -1,3 +1,1 @@
 export { default as Button } from './button'
-export { default as ButtonGroupCheckbox } from './button-group-checkbox'
-export { default as ButtonGroupRadio } from './button-group-radio'
