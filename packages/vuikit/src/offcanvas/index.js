897df7a22692ddbc13f919dfdf96146a2fe11541
Miljan Aleksic <miljan@aleksic.es>
2018-05-30 19:17:56 +0800

    export vk-offcanvas core for customizations

--- packages/vuikit/src/offcanvas/index.js
+++ packages/vuikit/src/offcanvas/index.js
@@ -1,6 +1,7 @@
 import * as constants from './constants'
 import { active } from './mixins/active'
+import core from './core'
 
-export { constants, active }
+export { constants, active, core }
 export * from './elements'
 export * from './components'
