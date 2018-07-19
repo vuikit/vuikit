e62399ae4790545389b1d5f9baf7c668aa3b354f
Miljan Aleksic <miljan@aleksic.es>
2018-07-19 10:38:35 +0200

    enhance props mixins

--- packages/vuikit/src/_core/mixins/props.js
+++ packages/vuikit/src/_core/mixins/props.js
@@ -1,9 +1,11 @@
 import { pick, get } from 'vuikit/src/_core/utils/object'
+import { extractProps } from 'vuikit/src/_core/utils/vue'
 
 export default {
   methods: {
-    pickComponentProps (obj, component) {
-      const props = get(this, `$options.components.${component}.props`, {})
+    pickComponentProps (obj, comp) {
+      const component = get(this, `$options.components.${comp}`, {})
+      const props = extractProps(component)
       return pick(obj, Object.keys(props))
     }
   }
